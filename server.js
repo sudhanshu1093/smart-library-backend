e358b10231ab2d758dc4bdd938cc75b67841d196
Sudhanshu <sudhanshukumar1093@gmail.com>
2026-03-05 15:46:46 +0530

    Fix browser CORS

--- server.js
+++ server.js
@@ -7,13 +7,18 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 
-app.use(cors({
-  origin: "https://sudhanshu1093.github.io",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-app.options("*", cors());
+// CORS Fix for browser requests
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
 
